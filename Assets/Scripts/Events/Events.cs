e2bc57f427544c740b899e24a3162c6f559ddf01
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2023-01-06 21:19:49 +0000

    Added event system

---
+++ Assets/Scripts/Events/Events.cs
@@ -0,0 +1,17 @@
+﻿public static class Events
+{
+
+}
+
+public class PlayerSpawnEvent : GameEvent
+{
+    public int maxHealth;
+}
+
+public class PlayerHitEvent : GameEvent
+{
+    public int currentHealth;
+    public int damageInflicted;
+}
+
+public class PlayerDeathEvent : GameEvent { }

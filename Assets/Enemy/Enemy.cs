13816121090d49ab364b5d4c0552cf9f64530ed1
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2023-01-06 22:04:35 +0000

    Created IDamagable interface and added basic enemies

---
+++ Assets/Enemy/Enemy.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour, IDamagable, IMoveable
+{
+    [field: SerializeField, Header("Movement")]
+    public float MoveSpeed { get; set; }
+
+
+    [field: SerializeField, Header("Health")]
+    public int MaxHealth { get; set; }
+    public int CurrentHealth { get; private set; }
+
+    [Header("Damage")]
+    [SerializeField] private int _damagePerHit = 20;
+
+    [SerializeField] private GameObject _target;
+
+    private EnemyManager _enemyManager;
+    private Rigidbody2D _rb2d;
+
+    private void Start()
+    {
+        CurrentHealth = MaxHealth;
+
+        _target = GameObject.Find("Player");
+        _enemyManager = FindObjectOfType<EnemyManager>();
+        _rb2d = GetComponent<Rigidbody2D>();
+
+        _enemyManager.RegisterEnemy(this);
+
+        _rb2d.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        Vector3 targetDir = _target.transform.position - transform.position;
+        targetDir.z = 0;
+        float angle = Mathf.Atan2(targetDir.x, targetDir.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, -transform.forward);
+
+        _rb2d.velocity = transform.up * MoveSpeed;
+    }
+
+    public void AddHealth(int amount)
+    {
+        CurrentHealth = Mathf.Clamp(CurrentHealth + amount, 0, MaxHealth);
+    }
+
+    public void RemoveHealth(int amount)
+    {
+        CurrentHealth = Mathf.Clamp(CurrentHealth - amount, 0, MaxHealth);
+
+        if (CurrentHealth <= 0) Die();
+    }
+    public void Die()
+    {
+        _enemyManager.DeregisterEnemy(this);
+
+        EnemyDeathEvent deathEvt = Events.s_EnemyDeathEvent;
+        deathEvt.xPos = transform.position.x;
+        deathEvt.yPos = transform.position.y;
+        EventManager.Broadcast(deathEvt);
+
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision == null) return;
+
+        if (collision.gameObject.CompareTag("Player")) return;
+
+        if (collision.gameObject.transform.TryGetComponent<IDamagable>(out var objectDamagable))
+        {
+            objectDamagable.RemoveHealth(_damagePerHit);
+        }
+        else
+        {
+            // Check parent
+            try
+            {
+                objectDamagable = collision.gameObject.transform.parent.GetComponent<IDamagable>();
+                objectDamagable.RemoveHealth(_damagePerHit);
+            }
+            catch
+            {
+                return;
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/IDamagable.cs
@@ -0,0 +1,10 @@
+﻿public interface IDamagable
+{
+    public int MaxHealth { get; set; }
+    public int CurrentHealth { get; }
+
+    public void RemoveHealth(int amount);
+    public void AddHealth(int amount);
+
+    public void Die();
+}

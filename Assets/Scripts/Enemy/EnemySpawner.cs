e178cebb631c58024872f526522bd1b080446c53
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2023-01-08 20:09:47 +0000

    Added enemy spawner objects

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    private const int MAX_ENEMIES = 200;
+
+    [SerializeField] private GameObject _enemyPrefab;
+
+    [SerializeField] private float _initialSpawnPeriod;
+    private float _currentSpawnPeriod;
+    private float _currentTime;
+
+    private EnemyManager _enemyManager;
+
+    private void Start()
+    {
+        // Give each spawner a random offset so they don't all spawn at the same time.
+        _currentTime = Random.value * _initialSpawnPeriod;
+        _currentSpawnPeriod = _initialSpawnPeriod;
+
+        _enemyManager = FindObjectOfType<EnemyManager>();
+    }
+
+    private void Update()
+    {
+        if (_currentTime >= _currentSpawnPeriod && CanSpawn())
+        {
+            Spawn();
+
+            _currentTime = 0.0f;
+            _currentSpawnPeriod -= 0.5f;
+        }
+
+        _currentTime += Time.deltaTime;
+    }
+
+    private bool CanSpawn()
+    {
+        return _enemyManager?.CurrentNumberOfEnemies < MAX_ENEMIES;
+    }
+
+    private void Spawn()
+    {
+        Instantiate(_enemyPrefab, transform.position, transform.rotation);
+    }
+}

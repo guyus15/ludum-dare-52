3e14b6c49e57545891f60d51aed993117b6ffbc9
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2023-01-08 23:01:19 +0000

    Added RadiusVisualiser

--- Assets/Scripts/Plants/HealingPlant.cs
+++ Assets/Scripts/Plants/HealingPlant.cs
@@ -4,17 +4,30 @@ public class HealingPlant : MonoBehaviour
 {
     [SerializeField] private float _timeBetweenHeals;
     [SerializeField] private int _healthPerHeal;
+    [SerializeField] private RadiusVisualiser _radiusVisualiser;
 
     private float _currentTime;
+    private CircleCollider2D _collider;
 
     private void Start()
     {
         _currentTime = 0.0f;
+        _collider = GetComponent<CircleCollider2D>();
+
+        _radiusVisualiser.SetRadius(_collider.radius);
     }
 
     private void Update()
     {
         _currentTime += Time.deltaTime;
+
+        if (_currentTime > _timeBetweenHeals )
+        {
+            _radiusVisualiser.SetColour(Color.green);
+        } else
+        {
+            _radiusVisualiser.SetColour(Color.red);
+        }
     }
 
     public void Harvest()
---
+++ Assets/Scripts/RadiusVisualiser.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class RadiusVisualiser : MonoBehaviour
+{
+    [SerializeField] private float _radius;
+
+    private int _segments = 50;
+    private LineRenderer _line;
+
+    void Start()
+    {
+        _line = gameObject.GetComponent<LineRenderer>();
+
+        _line.positionCount = _segments + 1;
+        _line.useWorldSpace = false;
+        CreatePoints();
+    }
+
+    void CreatePoints()
+    {
+        float angle = 20f;
+
+        for (int i = 0; i < (_segments + 1); i++)
+        {
+            float x = Mathf.Sin(Mathf.Deg2Rad * angle) * _radius;
+            float y = Mathf.Cos(Mathf.Deg2Rad * angle) * _radius;
+
+            _line.SetPosition(i, new Vector3(x, y, 0));
+
+            angle += (360f / _segments);
+        }
+    }
+
+    public void SetRadius(float radius)
+    {
+        _radius = radius;
+    }
+
+    public void SetColour(Color colour)
+    {
+        _line.startColor = colour;
+        _line.endColor = colour;
+    }
+}
